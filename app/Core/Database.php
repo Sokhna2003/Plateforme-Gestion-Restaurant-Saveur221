6c7cef562b411d5d1f4d82e3960978391238a4bb
sokhna <dianesokhna2003@gmail.com>
2026-09-11 18:17:53 +0000

    ajout de env et .htaccess pour le rewritting

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -23,18 +23,22 @@ class Database
     private function __construct()
     {
         $config = require __DIR__ . '/../../config/config.php';
-        $db = $config['db'];
+        // $db = $config['db'];
 
         try {
             $dsn = sprintf(
                 'mysql:host=%s;port=%d;dbname=%s;charset=%s',
-                $db['host'],
-                $db['port'],
-                $db['name'],
-                $db['charset']
+                $_ENV['DB_HOST'] ?? 'localhost',
+                $_ENV['DB_PORT'] ?? '3306',
+                $_ENV['DB_NAME'] ?? 'restaurant_saveur221',
+                $_ENV['DB_CHARSET'] ?? 'utf8mb4'
+                // $db['host'],
+                // $db['port'],
+                // $db['name'],
+                // $db['charset']
             );
 
-            $this->conn = new PDO($dsn, $db['user'], $db['password'], [
+            $this->conn = new PDO($dsn, $_ENV['DB_USER'] ?? 'root', $_ENV['DB_PASSWORD'] ?? '', [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 PDO::ATTR_EMULATE_PREPARES => false,
